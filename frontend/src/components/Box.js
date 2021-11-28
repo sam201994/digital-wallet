615af45567155c757936ecfbc64bd15f1563827b
Smriti Goel <smritigoel93@gmail.com>
2021-11-28 20:38:17 +0530

    added header comp

--- frontend/src/components/Box.js
+++ frontend/src/components/Box.js
@@ -14,14 +14,32 @@ const Auth = styled.div`
 	min-width: 220px;
 	display: flex;
 	flex-direction: column;
-	box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 	padding: 3rem;
 	border-radius: 0.5rem;
+`;
 
+const Header = styled.div`
+	margin-left: -3rem;
+	margin-right: -3rem;
+	padding-left: 3rem;
+	padding-right: 3rem;
+	margin-top: -3rem;
+	padding-top: 2rem;
+	display: flex;
+	justify-content: center;
+	overflow: hidden;
+	margin-bottom: 2rem;
+	border-top-left-radius: 0.5rem;
+	border-top-right-radius: 0.5rem;
+	height: 50px;
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+	font-size: 1.5rem;
 `;
 
 export default {
 	Row,
 	Page,
 	Auth,
+	Header,
 };
